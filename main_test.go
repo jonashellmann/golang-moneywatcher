aab2ed8453ad530b512e1e15581005ff9093f2d6
Gogs <gogs@fake.local>
2018-07-03 20:14:25 +0200

    Refactoring

--- main_test.go
+++ main_test.go
@@ -40,25 +40,6 @@ func TestRouter(t *testing.T) {
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Status should be ok, got %d", resp.StatusCode)
 	}
-
-	// In the next few lines, the response body is read, and converted to a string
-	/* defer resp.Body.Close()
-	// read the body into a bunch of bytes (b)
-	b, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
-	// convert the bytes to a string
-	respString := string(b)
-	expected := "Hello World!"
-
-	// We want our response to match the one defined in our handler.
-	// If it does happen to be "Hello world!", then it confirms, that the
-	// route is correct
-	if respString != expected {
-		t.Errorf("Response should be %s, got %s", expected, respString)
-	} */
-
 }
 
 func TestRouterForNonExistentRoute(t *testing.T) {
@@ -85,7 +66,6 @@ func TestRouterForNonExistentRoute(t *testing.T) {
 	if respString != expected {
 		t.Errorf("Response should be %s, got %s", expected, respString)
 	}
-
 }
 
 func TestStaticFileServer(t *testing.T) {
@@ -98,19 +78,14 @@ func TestStaticFileServer(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// We want our status to be 200 (ok)
 	if resp.StatusCode != http.StatusOK {
 		t.Errorf("Status should be 200, got %d", resp.StatusCode)
 	}
 
-	// It isn't wise to test the entire content of the HTML file.
-	// Instead, we test that the content-type header is "text/html; charset=utf-8"
-	// so that we know that an html file has been served
 	contentType := resp.Header.Get("Content-Type")
 	expectedContentType := "text/html; charset=utf-8"
 
 	if expectedContentType != contentType {
 		t.Errorf("Wrong content type, expected %s, got %s", expectedContentType, contentType)
 	}
-
 }
